4c5fab8a75e7df0a0811b994583d7968ace03af3
Makai Marcell <hello@makaimarcell.com>
2026-07-01 19:39:05 +0200

    [Fix] Update PaperScraper to use v3 API endpoints

--- src/scraper/scrapers/paper.scraper.ts
+++ src/scraper/scrapers/paper.scraper.ts
@@ -1,38 +1,50 @@
 import { createHash } from "crypto";
 
 import { validateJarVersion } from "src/utils/validate";
-import JarDto, { JarType } from "src/routes/jar/dto/jar.dto";
+import JarDto, { JarType } from "src/routes/jars/dto/jar.dto";
 import Scraper, { ScraperResult } from "./scraper";
 import DownloadHandler from "../handlers/download.handler";
 
+type ProjectVersions = Record<string, string[]>;
+
+type Project = {
+    project: {
+        id: string;
+        name: string;
+    };
+    versions: ProjectVersions;
+};
+
 export default class PaperScraper extends Scraper {
     public PROJECT_NAME: JarType = JarType.paper;
-    protected BASE_URL: string   = "https://api.papermc.io/v2/projects";
+    protected BASE_URL: string   = "https://fill.papermc.io/v3/projects";
 
     public async scrape(): Promise<ScraperResult[]>
     {
-        let res     = await fetch(`${this.BASE_URL}/${this.PROJECT_NAME}`);
-        let project = await res.json();
+        let res              = await fetch(`${this.BASE_URL}/${this.PROJECT_NAME}`);
+        let project: Project = await res.json();
 
         let list: ScraperResult[] = [];
 
-        for (let version of project.versions) {
-            if (! await validateJarVersion(version))
-                continue;
-            
-            let latest_build = await this.fetchLatestBuild(version);
-            
-            let jar = new JarDto();
-            jar.identifier = latest_build.identifier;
-            jar.type       = this.PROJECT_NAME;
-            jar.version    = version;
-            jar.fileName   = `${this.PROJECT_NAME}-${version}.jar`;
-            
-            list.push({
-                dto:         jar,
-                downloadUrl: latest_build.downloadUrl,
-                handler:     DownloadHandler
-            });
+        for (const [_, versions] of Object.entries(project.versions)) {
+            for (const version of versions) {
+                if (! await validateJarVersion(version))
+                    continue;
+                
+                let latest_build = await this.fetchLatestBuild(version);
+                
+                let jar = new JarDto();
+                jar.identifier = latest_build.identifier;
+                jar.type       = this.PROJECT_NAME;
+                jar.version    = version;
+                jar.fileName   = `${this.PROJECT_NAME}-${version}.jar`;
+                
+                list.push({
+                    dto:         jar,
+                    downloadUrl: latest_build.downloadUrl,
+                    handler:     DownloadHandler
+                });
+            }
         }
 
         return list;
@@ -40,20 +52,13 @@ export default class PaperScraper extends Scraper {
 
     protected async fetchLatestBuild(version: string)
     {    
-        let res  = await fetch(`${this.BASE_URL}/${this.PROJECT_NAME}/versions/${version}/builds`);
+        let res  = await fetch(`${this.BASE_URL}/${this.PROJECT_NAME}/versions/${version}/builds/latest`);
         let data = await res.json();
 
-        let latest_build = data.builds.pop();
-
         return {
-            identifier:  this.getIdentifier(version, latest_build.build),
-            downloadUrl: this.formatDownloadUrl(version, latest_build.build, latest_build.downloads.application.name)
+            identifier:  this.getIdentifier(version, data.id),
+            downloadUrl: data.downloads['server:default'].url,
         };
-    }
-
-    protected formatDownloadUrl(version: string, build_number: number, artifact_name: string): string
-    {
-        return `${this.BASE_URL}/${this.PROJECT_NAME}/versions/${version}/builds/${build_number}/downloads/${artifact_name}`;
     }
 
     public getIdentifier(version: string, build_number: number): string
