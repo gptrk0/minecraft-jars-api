8f6eceec6ad38cb8990227b8611e7ab852db191b
Makai Marcell <hello@makaimarcell.com>
2026-07-03 18:47:30 +0200

    [Add] Update version regex pattern for validation because of NeoForged versioning

--- src/routes/jar/dto/jar.dto.ts
+++ src/routes/jar/dto/jar.dto.ts
@@ -40,7 +40,7 @@ export default class JarDto
     type: JarType;
 
     @IsString()
-    @Matches(/^\d{1,2}(\.\d{1,2})?\.\d{1,2}$/, {
+    @Matches(/^\d{1,2}(?:\.\d{1,2}){2,3}$/, {
         groups: [ "version" ]
     })
     @IsNotEmpty()
